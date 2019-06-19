63fc7b81c50c040e679bfa6076249c17818ab202
Iryna Zaporozhets <iz2@rice.edu>
2019-06-19 13:28:28 -0500

    Added test for checking RDC calculations

---
+++ tests/test1/RDC.py
@@ -0,0 +1,50 @@
+import numpy as np
+import os
+import re
+import time
+import mdtraj as md
+import md_nmr2 as nmr2
+
+# Test 1. Average_mode of analysis 
+def test_rdc_average():
+    print("Testing function calculate_rdc_large with mode='average'")
+    path_traj = 'trajectory.xtc'
+    path_top  = 'topology.pdb'
+    RDC_inp_file = "experimental_data.txt" # Experimental data - allignment A28               
+    exp_rdc,D_av = nmr2.calculate_rdc_large(path_traj,
+                                                   path_top,
+                                                   RDC_inp_file,
+                                                   minimize_rmsd=False, # Always use the trajectory where RMSD is already minimized
+                                                   mode='average'
+                                                   )     
+    
+    reference = np.loadtxt("reference_calculated_average.txt")
+    difference  = np.subtract(D_av, reference)
+    print("Maximum difference between calculated and average value:  %f" %np.max(difference))
+    print("Mean difference between calculated and average value:     %f" %np.mean(difference))
+    return()
+
+
+def test_rdc_full():
+    print("Testing function calculate_rdc_large with mode='full'")
+    path_traj = 'trajectory.xtc'
+    path_top  = 'topology.pdb'
+    RDC_inp_file = "experimental_data.txt" # Experimental data - allignment A28               
+    exp_rdc,D_av = nmr2.calculate_rdc_large(path_traj,
+                                                   path_top,
+                                                   RDC_inp_file,
+                                                   minimize_rmsd=False, # Always use the trajectory where RMSD is already minimized
+                                                   mode='full'
+                                                   )     
+    
+    reference = np.loadtxt("reference_calculated_full.txt")
+    difference  = np.subtract(D_av, reference)
+    print("Maximum difference between calculated and average value:  %f" %np.max(difference))
+    print("Mean difference between calculated and average value:     %f" %np.mean(difference))
+    return()
+
+
+if __name__=='__main__':
+    test_rdc_average()
+    test_rdc_full()
+
---
+++ tests/test1/README.txt
@@ -0,0 +1,13 @@
+Experimantal data: RDC for ubuqiutin, conditions A28
+
+Reference calculated data were computed 
+on  june 19,  2019 using input files in this folder and function nmr2.calculate_rdc_large (version valid on june 19, 2019 at 13:06)
+
+
+Files needed to run the test:
+
+experimental_data.txt
+reference_calculated_average.txt  : Computation mode average
+reference_calculated_full.txt     : Computation mode full
+topology.pdb
+trajectory.xtc
