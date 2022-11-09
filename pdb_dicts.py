9236cd8d18baccc4d73bc5313d3e091c66cb854a
Iryna Zaporozhets <iryna@physik.fu-berlin.de>
2022-11-09 11:17:41 +0100

    Extend dict_of_heavy_atoms to include all 20 aminoacids

--- pdb_dicts.py
+++ pdb_dicts.py
@@ -5,24 +5,34 @@ structures and pdb files.
 
 """
 Dictionary dict_of_heavy_atoms maps 3-letter aminoacid code to the list of
-all heavy-atom namesas used in pdb files. Some of the aminoacids are not present in the dict.
+all heavy-atom namesas used in pdb files. S
+The names are taken from:
+    https://cdn.rcsb.org/wwpdb/docs/documentation/file-format/PDB_format_1992.pdf
+
 """
-dict_of_heavy_atoms = { "ALA" : ['N','CA','C','O','CB'],
-                        "VAL" : ['N','CA','C','O','CB','CG1','CG2'],
-                        "GLN" : ['N','CA','C','O','CB','CG','CD','OE1','NE2'],
-                        "GLY" : ['N','CA','C','O'],
-                        "ILE" : ['N','CA','C','O','CB','CG1','CG2','CD1'],
-                        "LEU" : ['N','CA','C','O','CB','CG','CD1','CD2'],
-                        "LYS" : ['N','CA','C','O','CB','CG','CD','CE','NZ'],
-                        "THR" : ['N','CA','C','O','CB','OG1','CG2'],
-                        "TYR" : ['N','CA','C','O','CB','CG','CD1','CD2','CE1','CE2','CZ','OH'],
-                        "ASN" : ['N','CA','C','O','CB','CG','OD1','ND2'],
-                        "MET" : ['N','CA','C','O','CB','CG','SD','CE'],
-                        "GLU" : ['N','CA','C','O','CB','CG','CD','OE1','OE2'],
-                        "ASP" : ['N','CA','C','O','CB','CG','OD1','OD2'],
-                        "PHE" : ['N','CA','C','O','CB','CG','CD1','CD2','CE1','CE2','CZ'],
-                        "TRP" : ['N','CA','C','O','CB','CG','CD1','CD2','NE1','CE2','CE3','CZ2','CZ3','CH2']
-    }
+backbone_atoms = ['N', 'CA', 'C', 'O']
+sidechain_atoms = { "ALA" : ['CB'],
+                    "ARG" : ['CB', 'CG', 'CD', 'NE', 'CZ', 'NH1', 'NH2'],
+                    "ASN" : ['CB', 'CG', 'OD1', 'ND2'],
+                    "ASP" : ['CB', 'CG', 'OD1', 'OD2'],
+                    "CYS" : ['CB', 'SG'],
+                    "GLU" : ['CB', 'CG', 'CD', 'OE1', 'OE2'],
+                    "GLN" : ['CB', 'CG', 'CD', 'OE1', 'NE2'],
+                    "GLY" : [],
+                    "HIS" : ['CB', 'CG', 'ND1', 'CD2', 'CE1', 'NE2'],
+                    "ILE" : ['CB', 'CG1', 'CG2', 'CD1'],
+                    "LEU" : ['CB', 'CG', 'CD1', 'CD2'],
+                    "LYS" : ['CB', 'CG', 'CD', 'CE', 'NZ'],
+                    "MET" : ['CB', 'CG', 'SD', 'CE'],
+                    "PHE" : ['CB', 'CG', 'CD1', 'CD2', 'CE1', 'CE2', 'CZ'],
+                    "PRO" : ['CB', 'CG', 'CD'],
+                    "SER" : ["CB", 'OG'],
+                    "TRE" : ['CB', 'OG1', 'CG2'],
+                    "TRP" : ['CB', 'CG', 'CD1', 'CD2', 'NE1', 'CE2', 'CE3', 'CZ2', 'CZ3', 'CH2'],
+                    "TYR" : ['CB', 'CG', 'CD1', 'CD2', 'CE1', 'CE2', 'CZ', 'OH'],
+                    "VAL" : ['CB', 'CG1', 'CG2']
+                    }
+dict_of_heavy_atoms = {item : backbone_atoms + value for item, value in sidechain_atoms.values() }
 
 
 """
